27e6149f66fbb99683654367c898388cea74f68c
Joshua Lu <lu.joshua.e@gmail.com>
2021-06-22 17:03:14 -0500

    added declaration for read_directory

--- include/wad/wadreader.h
+++ include/wad/wadreader.h
@@ -9,6 +9,7 @@ class wadreader
         ~wadreader();
 
         void read_header(const uint8_t *p_WADData, int offset, header &header);
+        void read_directory(const uint8_t *p_WADData, int offset, directory &directory);
 
     protected:
         uint16_t read2bytes(const uint8_t *p_WADData, int offset);
