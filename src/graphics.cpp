0afc0cae10213fe1b486679c024ea1b55ed7818a
Joshua Lu <lu.joshua.e@gmail.com>
2021-12-02 15:45:33 -0600

    change background color to black

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -87,7 +87,7 @@ void init_window()
 void display()
 {
    // Rendering
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
     shader.use();
--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,11 @@ int main(int argc, char** argv)
 {
     z_malloc_init(8);
     loadwad(argc, argv); // Gives the object the path to the WAD specified in the command line argument
+    void* block = loadmap("MAP01", VERTEXES);
+    Vertex* vertex = read_vertex(block);
     init_window();
     gameloop();
     glfwTerminate();
-    z_free(vertex);
     z_malloc_cleanup();
     return 0;
 }
